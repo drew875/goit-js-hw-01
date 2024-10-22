f564a1c647504c3f009d85648cf44bdf66356f12
Andrii Hordiienko <30300588+drew875@users.noreply.github.com>
2024-10-22 09:38:41 +0300

    first commit

---
+++ js/task-3.js
@@ -0,0 +1,7 @@
+function getElementWidth(content, padding, border) {
+
+    return content + padding + padding + border + border;
+}
+console.log(getElementWidth(50, 8, 4)); // 74
+console.log(getElementWidth(60, 12, 8.5)); // 101
+console.log(getElementWidth(200, 0, 0)); // 200
